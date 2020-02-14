fa7e370d60ddba9411598ac92ae17cad496a9747
Fellipe Adorno <fellipeac2@gmail.com>
2020-02-14 13:41:40 -0300

    refactor(angular-sdk): inject base_url to library Change the way we use base_url to inject a string in library

--- projects/angular-sdk/src/lib/angular-sdk.module.ts
+++ projects/angular-sdk/src/lib/angular-sdk.module.ts
@@ -1,10 +1,39 @@
-import { NgModule } from '@angular/core'
+import { NgModule, InjectionToken } from '@angular/core'
 import { CoreModule } from './core/core.module'
 import { HttpClientModule } from '@angular/common/http'
+
+export const BASE_URL = new InjectionToken<string>('BASE_URL')
 
 @NgModule({
     declarations: [],
     imports: [CoreModule, HttpClientModule],
     exports: [CoreModule]
 })
-export class AngularSdkModule {}
+export class AngularSdkModule {
+    static forRoot(env_str: string) {
+        let base_url
+        switch (env_str) {
+            case 'beta':
+                base_url = 'https://api-beta.musicplayce.com'
+                break
+            case 'sandbox':
+                base_url = 'https://api-sandbox.musicplayce.com'
+                break
+            case 'dev':
+                base_url = 'http://localhost:3000'
+                break
+            default:
+                base_url = 'https://api.musicplayce.com'
+                break
+        }
+        return {
+            ngModule: AngularSdkModule,
+            providers: [
+                {
+                    provide: BASE_URL,
+                    useValue: base_url
+                }
+            ]
+        }
+    }
+}
--- projects/angular-sdk/src/lib/angular-sdk.service.ts
+++ projects/angular-sdk/src/lib/angular-sdk.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@angular/core'
+import { Injectable, Inject } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
+import { BASE_URL } from './angular-sdk.module'
 
 @Injectable({
     providedIn: 'root'
 })
 export class AngularSdkService {
-    constructor(private httpClient: HttpClient) {}
+    public BASE_URL: string
+    constructor(
+        @Inject(BASE_URL) base_url_injected: string,
+        private httpClient: HttpClient
+    ) {
+        this.BASE_URL = base_url_injected
+    }
 
     public convert(value: string): string {
         return `-- ${value} --`
     }
 
     public getAuth(): Observable<any> {
-        return this.httpClient.get(
-            'https://api-sandbox.musicplayce.com/v1/auth/me/'
-        )
+        return this.httpClient.get(this.BASE_URL + '/v1/auth/me/')
     }
 }
