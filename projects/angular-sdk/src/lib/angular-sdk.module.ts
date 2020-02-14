c522c7755a90e6b513075d873957b724c9be0a5b
Fellipe Adorno <fellipeac2@gmail.com>
2020-02-14 15:20:46 -0300

    refactor(angular-sdk): add domain_cookie injector

--- projects/angular-sdk/src/lib/angular-sdk.module.ts
+++ projects/angular-sdk/src/lib/angular-sdk.module.ts
@@ -3,6 +3,7 @@ import { CoreModule } from './core/core.module'
 import { HttpClientModule } from '@angular/common/http'
 
 export const BASE_URL = new InjectionToken<string>('BASE_URL')
+export const COOKIE_DOMAIN = new InjectionToken<string>('CO0KIE_DOMAIN')
 
 @NgModule({
     declarations: [],
@@ -11,19 +12,28 @@ export const BASE_URL = new InjectionToken<string>('BASE_URL')
 })
 export class AngularSdkModule {
     static forRoot(env_str: string) {
-        let base_url
+        let base_url: string
+        let cookie_domain: string
         switch (env_str) {
             case 'beta':
                 base_url = 'https://api-beta.musicplayce.com'
+                cookie_domain = '.musicplayce.com'
                 break
             case 'sandbox':
                 base_url = 'https://api-sandbox.musicplayce.com'
+                cookie_domain = '.musicplayce.com'
                 break
             case 'dev':
                 base_url = 'http://localhost:3000'
+                cookie_domain = 'localhost'
+                break
+            case 'test':
+                base_url = 'https://api.musicplayce.com'
+                cookie_domain = 'localhost'
                 break
             default:
                 base_url = 'https://api.musicplayce.com'
+                cookie_domain = '.musicplayce.com'
                 break
         }
         return {
@@ -32,6 +42,10 @@ export class AngularSdkModule {
                 {
                     provide: BASE_URL,
                     useValue: base_url
+                },
+                {
+                    provide: COOKIE_DOMAIN,
+                    useValue: cookie_domain
                 }
             ]
         }
