d5bc95a18e27d52437c13c42899ab062323acf29
Fellipe Adorno <fellipeac2@gmail.com>
2020-02-19 16:59:09 -0300

    fix(js-sha256): change crypt password method

--- projects/angular-sdk/src/lib/core/service/auth.service.ts
+++ projects/angular-sdk/src/lib/core/service/auth.service.ts
@@ -10,10 +10,8 @@ import { ApiRequest, DataRequest } from './models/request.base.model'
 import { environment } from '../../../environments/environment'
 import { RefreshSendModel } from './models/refresh.send.model'
 import { CookieUtil } from '../util/cookie.util'
-import { encode } from 'utf8'
-import { SHA256 } from 'crypto-js'
+import { sha256 } from 'js-sha256'
 import { tap, concatMap, map, catchError } from 'rxjs/operators'
-
 import { BASE_URL, COOKIE_DOMAIN } from '../../angular-sdk.module'
 
 @Injectable({
@@ -136,8 +134,7 @@ export class AuthService {
      * @returns Observable<AuthResponse> an observable with tokens in AuthResponse model message
      */
     public login(user: UserAuth): Observable<AuthResponse> {
-        let passUtf8 = encode(user.password)
-        let passEncode = SHA256(passUtf8)
+        let passEncode = sha256(user.password)
         user.password = passEncode.toString()
         let body = new ApiRequest()
 
@@ -191,8 +188,7 @@ export class AuthService {
         accepted_policy?: number,
         accepted_terms?: number
     ): Observable<AuthResponse> {
-        let passUtf8 = encode(password)
-        let passEncode = SHA256(passUtf8)
+        let passEncode = sha256(password)
         password = passEncode.toString()
 
         let params = {
