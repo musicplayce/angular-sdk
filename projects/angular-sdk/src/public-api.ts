a587c8e39aea8c4ee4910f3537e4a1fbbcffea6f
fellipeac2 <fellipeac2@gmail.com>
2020-03-24 14:57:42 -0300

    Update public-api.ts

--- projects/angular-sdk/src/public-api.ts
+++ projects/angular-sdk/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './lib/angular-sdk.module'
+export * from './lib/services/subscriptions.service'
 export * from './lib/core/core.module'
 export * from './lib/core/service/auth.service'
 export * from './lib/core/service/models/user.auth.model'
