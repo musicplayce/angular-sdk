15ca41b341fb3cf65a9fa62669753919d12405eb
Fellipe Adorno <fellipe.adorno@musicplayce.com>
2020-04-13 17:20:02 -0300

    fix(public-ts): add audition.service

--- projects/angular-sdk/src/public-api.ts
+++ projects/angular-sdk/src/public-api.ts
@@ -4,7 +4,7 @@
 
 export * from './lib/angular-sdk.module'
 export * from './lib/services/subscriptions.service'
+export * from './lib/services/auditions.service'
 export * from './lib/core/core.module'
 export * from './lib/core/service/auth.service'
 export * from './lib/core/service/models/user.auth.model'
-export * from './lib/core/service/auth.service'
