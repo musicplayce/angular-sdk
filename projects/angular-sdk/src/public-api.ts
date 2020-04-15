a6a7c9f76725b98fb040bb42c9b907e2dd63f492
Jean Santos da Costa <jean.mscosta@gmail.com>
2020-04-15 13:30:38 -0300

    feat(playlistsservice): add playlists.service

--- projects/angular-sdk/src/public-api.ts
+++ projects/angular-sdk/src/public-api.ts
@@ -3,8 +3,9 @@
  */
 
 export * from './lib/angular-sdk.module'
-export * from './lib/services/subscriptions.service'
 export * from './lib/services/auditions.service'
+export * from './lib/services/playlists.service'
+export * from './lib/services/subscriptions.service'
 export * from './lib/core/core.module'
 export * from './lib/core/service/auth.service'
 export * from './lib/core/service/models/user.auth.model'
