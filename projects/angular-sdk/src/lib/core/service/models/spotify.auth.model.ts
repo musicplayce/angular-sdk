3f681398fa4fa53e9c5eaa7468f1e8571c77bd00
fellipeac2 <fellipeac2@gmail.com>
2020-05-14 19:53:04 -0300

    fix(sotifyauthmodel): accepted terms
    Add accepted terms policy and marketing in credentials

--- projects/angular-sdk/src/lib/core/service/models/spotify.auth.model.ts
+++ projects/angular-sdk/src/lib/core/service/models/spotify.auth.model.ts
@@ -2,9 +2,15 @@ export class SpotifyAuth {
     code: string
     redirect_uri: string
     device_token: string
+    accepted_terms: number
+    accepted_policy: number
+    accepted_marketing: number
 
-    constructor(code: string, redirect_uri: string) {
+    constructor(code: string, redirect_uri: string, accepted_terms: number, accepted_policy: number, accepted_marketing: number) {
         this.code = code
         this.redirect_uri = redirect_uri
+        this.accepted_terms = accepted_terms
+        this.accepted_policy = accepted_policy
+        this.accepted_marketing = accepted_marketing
     }
 }
